2c92a98f6654ab84e769121ac06e90bfab707aa7
saimtiaz <saymimtiaz@gmail.com>
2022-07-06 13:03:05 -0500

    Skeleton viz code

---
+++ catkin_ws/src/cluster/src/clusterViz.py
@@ -0,0 +1,34 @@
+import cluster2obj as clus
+import time
+import os
+
+
+def kmeansViz():
+    return
+
+def euclideanViz():
+    return
+
+def boxViz():
+    #Given a cluster, return a viz 
+    return
+
+def sphereViz():
+    #Given a cluster, return a viz of the cluster and the sphere itself
+    return
+
+def boxOrSphereViz():
+    #Given a specific cluster, show it being represented by either a box or a sphere
+    #Will also would want to show how the full representation looks like with each strategy
+    #tablescene may be a good idea
+    return
+
+def outlierViz():
+    return
+
+def kTreeViz():
+    return
+
+if __name__ == "__main__":
+    pointCloudDir = '..\\..\\..\\..\\pointClouds'
+    fileList = os.listdir(pointCloudDir)
